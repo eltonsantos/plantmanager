347aed143a9b7bc17ebc79d3399c14b8f7858a87
Elton Santos <eltonaxl@hotmail.com>
2021-04-24 22:36:10 -0300

    Finish Day 3

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
-import { StyleSheet, SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+import { useNavigation } from '@react-navigation/core';
+
 import { Button } from '../components/Button';
 
 export function Confirmation() {
-  return (
+  const navigation = useNavigation();
+  
+  function handleMoveOn() {
+    navigation.navigate('PlantSelect');
+  }
+
+  return(
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
         <Text style={styles.emoji}>
@@ -20,17 +28,16 @@ export function Confirmation() {
         </Text>
 
         <Text style={styles.subtitle}>
-          Agora vamos começar a cuidar das suas
+          Agora vamos começar a cuidar das suas {'\n'}
           plantinhas com muito cuidado.
         </Text>
 
         <View style={styles.footer}>
-          <Button title="Começar" />
+          <Button title="Começar" onPress={handleMoveOn} />
         </View>
-
       </View>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
